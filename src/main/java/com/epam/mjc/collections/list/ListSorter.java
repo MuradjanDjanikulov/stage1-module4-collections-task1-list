e62c4e54aa44a870c4860366cc1caabfd9eb62a3
murod <murad.djanikulov@gmail.com>
2024-02-24 19:45:40 +0500

    list task1 done

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,24 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int aValue = calculateFunctionValue(Integer.parseInt(a));
+        int bValue = calculateFunctionValue(Integer.parseInt(b));
+
+        if (aValue != bValue) {
+            return Integer.compare(aValue, bValue);
+        } else {
+            return Integer.compare(Integer.parseInt(a), Integer.parseInt(b));
+        }
+    }
+
+    private int calculateFunctionValue(int x) {
+        return 5 * x * x + 3;
     }
 }
